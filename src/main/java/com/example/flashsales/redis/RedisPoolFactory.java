f1d3ea45df4fb7c357d84ef10e415b0c291a446e
Tianyi Zhou <zcarol.530@gmail.com>
2021-09-19 01:02:07 -0400

    added Redis service

--- src/main/java/com/example/flashsales/redis/RedisPoolFactory.java
+++ src/main/java/com/example/flashsales/redis/RedisPoolFactory.java
@@ -1,2 +1,30 @@
-package com.example.flashsales.redis;public class RedisPoolFactory {
+package com.example.flashsales.redis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+@Service
+public class RedisPoolFactory {
+
+    @Autowired
+    RedisConfig redisConfig;
+
+    @Bean
+    public JedisPool JedisPoolFactory(){
+        System.out.println("redis config host: " + redisConfig.getHost());
+
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+
+
+        poolConfig.setMaxIdle(redisConfig.getPoolMaxIdle());
+        poolConfig.setMaxTotal(redisConfig.getPoolMaxTotal());
+        poolConfig.setMaxWaitMillis(redisConfig.getPoolMaxWait() * 1000);
+
+        JedisPool jp = new JedisPool(poolConfig, redisConfig.getHost(), redisConfig.getPort(),
+                redisConfig.getTimeout() * 1000, redisConfig.getPassword(), 0);
+        return  jp;
+    }
 }
--- src/main/java/com/example/flashsales/redis/RedisService.java
+++ src/main/java/com/example/flashsales/redis/RedisService.java
@@ -1,2 +1,87 @@
-package com.example.flashsales.redis;public class RedisService {
+package com.example.flashsales.redis;
+
+import com.alibaba.fastjson.JSON;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+
+
+@Service
+public class RedisService {
+
+    @Autowired
+    JedisPool jedisPool;
+
+
+
+    public <T> T get(KeyPrefix prefix, String key, Class<T> clazz){
+        Jedis jedis = null;
+        try{
+            jedis = jedisPool.getResource();
+            String actualKey = prefix.getPrefix() + key;
+            String str = jedis.get(actualKey);
+            T t = stringToBean(str, clazz);
+            return t;
+        } finally {
+            returnToPool(jedis);
+        }
+    }
+
+    public <T> boolean set(KeyPrefix prefix, String key, T value){
+        Jedis jedis = null;
+        try{
+            jedis = jedisPool.getResource();
+            String str = beanToString(value);
+            if (str == null || str.length() <= 0){
+                return false;
+            }
+            String actualKey = prefix.getPrefix() + key;
+            jedis.set(actualKey,str);
+            return true;
+        } finally {
+            returnToPool(jedis);
+        }
+    }
+
+    private <T> String beanToString(T value) {
+        if(value == null){
+            return null;
+        }
+        Class<?> clazz = value.getClass();
+        if (clazz == int.class || clazz == Integer.class){
+            return "" + value;
+        } else  if (clazz == String.class){
+            return  (String)value;
+        } else if (clazz == long.class || clazz == Long.class){
+            return "" + value;
+        } else {
+            return JSON.toJSONString(value);
+        }
+    }
+
+    private <T> T stringToBean(String str, Class<T> clazz) {
+        if (str == null || str.length() <= 0 || clazz == null){
+            return null;
+        }
+        if (clazz == int.class || clazz == Integer.class){
+            return (T)Integer.valueOf(str);
+        } else  if (clazz == String.class){
+            return (T)str;
+        } else if (clazz == long.class || clazz == Long.class){
+            return (T)Long.valueOf(str);
+        } else {
+            return JSON.toJavaObject(JSON.parseObject(str), clazz);
+        }
+    }
+
+    private void returnToPool(Jedis jedis) {
+        if (jedis != null){
+            jedis.close();
+        }
+    }
+
+
+
 }
