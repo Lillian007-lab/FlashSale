b4c0caefe52c17a266727bce7cf86019f553c08a
Tianyi Zhou <zcarol.530@gmail.com>
2021-09-20 01:40:49 -0400

    added getByToken

--- src/main/java/com/example/flashsales/service/FlashSalesUserService.java
+++ src/main/java/com/example/flashsales/service/FlashSalesUserService.java
@@ -3,23 +3,36 @@ package com.example.flashsales.service;
 import com.example.flashsales.Exception.GlobalException;
 import com.example.flashsales.dao.FlashSalesUserDAO;
 import com.example.flashsales.domain.FlashSalesUser;
+import com.example.flashsales.redis.FlashSalesUserKey;
+import com.example.flashsales.redis.RedisConfig;
+import com.example.flashsales.redis.RedisService;
 import com.example.flashsales.result.CodeMsg;
 import com.example.flashsales.util.MD5Util;
+import com.example.flashsales.util.UUIDUtil;
 import com.example.flashsales.vo.LoginVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.util.StringUtils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @Service
 public class FlashSalesUserService {
 
+    public static final String COOKIE_NAME_TOKEN = "token";
+
     @Autowired
     FlashSalesUserDAO flashSalesUserDAO;
+
+    @Autowired
+    RedisService redisService;
 
     public FlashSalesUser getById(long id){
         return flashSalesUserDAO.getById(id);
     }
 
-    public boolean login(LoginVo loginVo) {
+    public boolean login(HttpServletResponse response, LoginVo loginVo) {
 
         if(loginVo == null){
             throw new GlobalException(CodeMsg.SERVER_ERROR);
@@ -38,6 +51,33 @@ public class FlashSalesUserService {
         if (calcPass.equals(dbPass)){
             throw new GlobalException(CodeMsg.PASSWORD_ERROR);
         }
+
+        // generate cookie
+        addCookie(response, flashSalesUser);
+
         return true;
     }
+
+    public FlashSalesUser getByToken(HttpServletResponse response, String token) {
+        if (StringUtils.isEmpty(token)){
+            return null;
+        }
+        FlashSalesUser flashSalesUser = redisService.get(FlashSalesUserKey.token, token, FlashSalesUser.class);
+        if (flashSalesUser != null){
+            addCookie(response, flashSalesUser);
+        }
+        return flashSalesUser;
+
+    }
+
+    private void addCookie(HttpServletResponse response, FlashSalesUser user) {
+        String token = UUIDUtil.uuid();
+        redisService.set(FlashSalesUserKey.token, token, user);
+        Cookie cookie = new Cookie(COOKIE_NAME_TOKEN, token);
+        cookie.setMaxAge(FlashSalesUserKey.token.expireSeconds());
+        cookie.setPath("/");
+        response.addCookie(cookie);
+    }
 }
+
+
