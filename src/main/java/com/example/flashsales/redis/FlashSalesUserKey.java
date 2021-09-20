c7544a448ca11f511223b2c45ec2165d5ebc38c1
Tianyi Zhou <zcarol.530@gmail.com>
2021-09-20 01:33:10 -0400

    added FlashSalesUserKey

--- src/main/java/com/example/flashsales/redis/FlashSalesUserKey.java
+++ src/main/java/com/example/flashsales/redis/FlashSalesUserKey.java
@@ -1,2 +1,12 @@
-package com.example.flashsales.redis;public class FlashSalesUserKey {
+package com.example.flashsales.redis;
+
+public class FlashSalesUserKey extends BasePrefix{
+
+    public static final int TOKEN_EXPIRE = 36000 * 24 * 2;
+
+    public FlashSalesUserKey(int expireSeconds, String prefix) {
+        super(expireSeconds, prefix);
+    }
+
+    public static FlashSalesUserKey token = new FlashSalesUserKey(TOKEN_EXPIRE,"tokenKey");
 }
