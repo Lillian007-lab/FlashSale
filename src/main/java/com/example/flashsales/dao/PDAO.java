d4bd327e358c5a14d6945f38625e668fe0c82c03
Tianyi Zhou <zcarol.530@gmail.com>
2021-09-23 00:27:30 -0400

    added product service

--- src/main/java/com/example/flashsales/dao/PDAO.java
+++
@@ -1,2 +0,0 @@
-package com.example.flashsales.dao;public class PDAO {
-}
--- src/main/java/com/example/flashsales/service/ProductService.java
+++ src/main/java/com/example/flashsales/service/ProductService.java
@@ -1,2 +1,20 @@
-package com.example.flashsales.service;public class ProductService {
+package com.example.flashsales.service;
+
+import com.example.flashsales.dao.ProductDAO;
+import com.example.flashsales.vo.ProductVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    ProductDAO productDAO;
+
+    public List<ProductVo> listProductVo(){
+        return productDAO.listProductVo();
+    }
+
 }
