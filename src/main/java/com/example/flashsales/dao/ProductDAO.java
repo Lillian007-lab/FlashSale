5362b2ce1af0d912d0215da3c28e840bed2fab83
Tianyi Zhou <zcarol.530@gmail.com>
2021-09-23 00:28:04 -0400

    added Product DAO

--- src/main/java/com/example/flashsales/dao/ProductDAO.java
+++ src/main/java/com/example/flashsales/dao/ProductDAO.java
@@ -1,2 +1,14 @@
-package com.example.flashsales.dao;public class ProductDAO {
+package com.example.flashsales.dao;
+
+import com.example.flashsales.vo.ProductVo;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface ProductDAO {
+
+    @Select("select p.*,fp.flash_sales_price,fp.flash_sales_stock,fp.start_date,fp.end_date from flash_sales_product fp left join product p on fp.product_id = p.id")
+    public List<ProductVo> listProductVo();
 }
