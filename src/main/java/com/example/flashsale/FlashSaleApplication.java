d580a5f88295b68935758a5b90155eca2f0cd594
Tianyi Zhou <zcarol.530@gmail.com>
2021-09-23 23:53:43 -0400

    updated

--- src/main/java/com/example/flashsale/FlashSaleApplication.java
+++ src/main/java/com/example/flashsale/FlashSaleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FlashSalesApplication {
+public class FlashSaleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FlashSalesApplication.class, args);
+		SpringApplication.run(FlashSaleApplication.class, args);
 	}
 
 }
