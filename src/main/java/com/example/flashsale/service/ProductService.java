eea4fb48531d605c7796afb216b32d7226ca3b57
Tianyi Zhou <zcarol.530@gmail.com>
2021-09-25 19:00:14 -0400

    added reduceStock method in service

--- src/main/java/com/example/flashsale/service/ProductService.java
+++ src/main/java/com/example/flashsale/service/ProductService.java
@@ -1,11 +1,15 @@
 package com.example.flashsale.service;
 
 import com.example.flashsale.dao.ProductDAO;
+import com.example.flashsale.domain.FlashSaleProduct;
+import com.example.flashsale.domain.Product;
 import com.example.flashsale.vo.ProductVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.spring5.util.SpringRequestUtils;
 
 import java.util.List;
+import java.util.PropertyPermission;
 
 @Service
 public class ProductService {
@@ -19,5 +23,12 @@ public class ProductService {
 
     public ProductVo getProductVoByProductId(long productId) {
         return productDAO.getProductVoByProductId(productId);
+    }
+
+    public void reduceStock(ProductVo productVo) {
+        FlashSaleProduct product = new FlashSaleProduct();
+        product.setProductId(productVo.getId());
+        productDAO.reduceStock(product);
+        System.out.println("stock is reduced");
     }
 }
