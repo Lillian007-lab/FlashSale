1d7089b3e79126536b88829d9603f2f565ca2ab8
Tianyi Zhou <zcarol.530@gmail.com>
2021-09-29 14:54:29 -0400

    updated FlashSaleController to apply web caching

--- src/main/java/com/example/flashsale/controller/FlashSaleController.java
+++ src/main/java/com/example/flashsale/controller/FlashSaleController.java
@@ -5,6 +5,7 @@ import com.example.flashsale.domain.FlashSaleUser;
 import com.example.flashsale.domain.Order;
 import com.example.flashsale.redis.RedisService;
 import com.example.flashsale.result.CodeMsg;
+import com.example.flashsale.result.Result;
 import com.example.flashsale.service.FlashSaleService;
 import com.example.flashsale.service.OrderService;
 import com.example.flashsale.service.ProductService;
@@ -14,7 +15,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.naming.ldap.SortResponseControl;
 
@@ -48,20 +51,21 @@ public class FlashSaleController {
      * @param productId
      * @return
      */
-    @RequestMapping("do_flash_sale")
-    public String doFlashSale(Model model, FlashSaleUser user,
+    @RequestMapping(value = "do_flash_sale", method = RequestMethod.POST)
+    @ResponseBody
+    public Result<Order> doFlashSale(Model model, FlashSaleUser user,
                               @RequestParam("productId")long productId){
         model.addAttribute("user", user);
         if (user == null){
-            return "login";
+            return Result.error(CodeMsg.SESSION_ERROR);
         }
 
         // check if the product is in stock
         ProductVo productVo = productService.getProductVoByProductId(productId);
         int stock = productVo.getFlashSaleStock();
         if (stock <= 0){
-            model.addAttribute("errmsg", CodeMsg.FLASH_SALE_OUT_OF_STOCK.getMsg());
-            return "flash_sale_fail";
+            //model.addAttribute("errmsg", CodeMsg.FLASH_SALE_OUT_OF_STOCK.getMsg());
+            return Result.error(CodeMsg.FLASH_SALE_OUT_OF_STOCK);
         }
         System.out.println("stock: " + stock);
 
@@ -69,15 +73,15 @@ public class FlashSaleController {
         // A user can't place multiple orders of a same flash sale product
         FlashSaleOrder flashSaleOrder = orderService.getFlashSaleOrderByUserIdProductId(user.getId(), productId);
         if (flashSaleOrder != null){
-            model.addAttribute("errmsg", CodeMsg.FLASH_SALE_REPEAT.getMsg());
-            return "flash_sale_fail";
+            //model.addAttribute("errmsg", CodeMsg.FLASH_SALE_REPEAT.getMsg());
+            return Result.error(CodeMsg.FLASH_SALE_REPEAT);
         }
 
         // decrease stock, place order, add to flash sale order
         Order order = flashSaleService.doFlashSale(user, productVo);
-        model.addAttribute("order", order);
-        model.addAttribute("product", productVo);
+//        model.addAttribute("order", order);
+//        model.addAttribute("product", productVo);
 
-        return "order_detail";
+        return Result.success(order);
     }
 }
