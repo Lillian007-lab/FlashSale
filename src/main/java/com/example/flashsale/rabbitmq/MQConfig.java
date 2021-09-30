42312f90ec8b63d934553dee38272c65b97cc98a
Tianyi Zhou <zcarol.530@gmail.com>
2021-09-30 17:29:11 -0400

    added Header Exchange

--- src/main/java/com/example/flashsale/rabbitmq/MQConfig.java
+++ src/main/java/com/example/flashsale/rabbitmq/MQConfig.java
@@ -1,8 +1,13 @@
 package com.example.flashsale.rabbitmq;
 
+import com.rabbitmq.client.AMQP;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.amqp.core.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Configuration
@@ -11,10 +16,13 @@ public class MQConfig {
     public static final String QUEUE = "queue";
     public static final String TOPIC_QUEUE1 = "topic.queue1";
     public static final String TOPIC_QUEUE2 = "topic.queue2";
-    public static final String TOPIC_EXCHANGE = "topic.exchange";
+    public static final String TOPIC_EXCHANGE = "topicExchange";
     public static final String ROUTING_KEY1 = "topic.key1";
     public static final String ROUTING_KEY2 = "topic.#";
-    public static final String FANOUT = "fanout";
+    public static final String FANOUT_EXCHANGE = "fanoutExchange";
+    public static final String HEADERS_EXCHANGE = "headersExchange";
+    public static final String HEADERS_QUEUE = "headers.queue";
+
 
 
 
@@ -66,7 +74,7 @@ public class MQConfig {
      */
     @Bean
     public FanoutExchange fanoutExchange() {
-        return  new FanoutExchange(FANOUT);
+        return  new FanoutExchange(FANOUT_EXCHANGE);
     }
 
     @Bean
@@ -77,5 +85,29 @@ public class MQConfig {
     @Bean
     public Binding fanoutBinding2() {
         return BindingBuilder.bind(topicQueue2()).to(fanoutExchange());
+    }
+
+
+    /**
+     * Header Exchange
+     *
+     * @return
+     */
+    @Bean
+    public HeadersExchange headersExchange() {
+        return new HeadersExchange(HEADERS_EXCHANGE);
+    }
+
+    @Bean
+    public Queue headerQueue1() {
+        return new Queue(HEADERS_QUEUE, true);
+    }
+
+    @Bean
+    public Binding headerBinding() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("header1", "value1");
+        map.put("header2", "value2");
+        return BindingBuilder.bind(headerQueue1()).to(headersExchange()).whereAll(map).match();
     }
 }
