7cce3091b4b79a6c079c5d797270c12043cd5196
Tianyi Zhou <zcarol.530@gmail.com>
2021-09-30 16:13:26 -0400

    added MQConfig

--- src/main/java/com/example/flashsale/rabbitmq/MQConfig.java
+++ src/main/java/com/example/flashsale/rabbitmq/MQConfig.java
@@ -1,2 +1,22 @@
-package com.example.flashsale.rabbitmq;public class MQConfig {
+package com.example.flashsale.rabbitmq;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class MQConfig {
+
+    public static final String QUEUE = "queue";
+
+    /**
+     * Direct Exchange
+     *
+     * @return
+     */
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE, true);
+    }
 }
