f6eaca906b4bb306f412766222689e6f643b4ebe
Tianyi Zhou <zcarol.530@gmail.com>
2021-09-30 16:14:00 -0400

    added MQReceiver and MQSender

--- src/main/java/com/example/flashsale/rabbitmq/MQReceiver.java
+++ src/main/java/com/example/flashsale/rabbitmq/MQReceiver.java
@@ -1,2 +1,17 @@
-package com.example.flashsale.rabbitmq;public class MQReceiver {
+package com.example.flashsale.rabbitmq;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MQReceiver {
+
+    private static Logger logger = LoggerFactory.getLogger(MQSender.class);
+
+    @RabbitListener(queues = MQConfig.QUEUE)
+    public void receive (String message){
+        logger.info("receive message: " + message);
+    }
 }
--- src/main/java/com/example/flashsale/rabbitmq/MQSender.java
+++ src/main/java/com/example/flashsale/rabbitmq/MQSender.java
@@ -1,2 +1,23 @@
-package com.example.flashsale.rabbitmq;public class MQSender {
+package com.example.flashsale.rabbitmq;
+
+import com.example.flashsale.redis.RedisService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MQSender {
+
+    private static Logger logger = LoggerFactory.getLogger(MQSender.class);
+
+    @Autowired
+    AmqpTemplate amqpTemplate;
+
+    public void send(Object message){
+        String msg = RedisService.beanToString(message);
+        logger.info("send message: " + msg);
+        amqpTemplate.convertAndSend(MQConfig.QUEUE, msg);
+    }
 }
