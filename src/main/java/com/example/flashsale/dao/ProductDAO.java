eb621adff155baec2cf8700efaf3161d7d831bee
Tianyi Zhou <zcarol.530@gmail.com>
2021-09-29 21:24:07 -0400

    update sql to avoid negative stock during load testing

--- src/main/java/com/example/flashsale/dao/ProductDAO.java
+++ src/main/java/com/example/flashsale/dao/ProductDAO.java
@@ -19,6 +19,6 @@ public interface ProductDAO {
     @Select("select p.*,fp.flash_sale_price,fp.flash_sale_stock,fp.start_date,fp.end_date from flash_sale_product fp left join product p on fp.product_id = p.id where p.id = #{productId}")
     ProductVo getProductVoByProductId(@Param("productId") long productId);
 
-    @Update("update flash_sale_product set flash_sale_stock = flash_sale_stock - 1 where product_id = #{productId}")
+    @Update("update flash_sale_product set flash_sale_stock = flash_sale_stock - 1 where product_id = #{productId} and flash_sale_stock > 0")
     public int reduceStock(FlashSaleProduct product);
 }
