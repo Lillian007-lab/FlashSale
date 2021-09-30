df51ea449d2cdb5ae64655db0630444af34b71da
Tianyi Zhou <zcarol.530@gmail.com>
2021-09-30 16:34:43 -0400

    added Topic Exchange in MQConfig

--- src/main/java/com/example/flashsale/rabbitmq/MQConfig.java
+++ src/main/java/com/example/flashsale/rabbitmq/MQConfig.java
@@ -1,6 +1,9 @@
 package com.example.flashsale.rabbitmq;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +12,13 @@ import org.springframework.context.annotation.Configuration;
 public class MQConfig {
 
     public static final String QUEUE = "queue";
+    public static final String TOPIC_QUEUE1 = "topic.queue1";
+    public static final String TOPIC_QUEUE2 = "topic.queue2";
+    public static final String TOPIC_EXCHANGE = "topic.exchange";
+    public static final String ROUTING_KEY1 = "topic.key1";
+    public static final String ROUTING_KEY2 = "topic.#";
+
+
 
     /**
      * Direct Exchange
@@ -18,5 +28,35 @@ public class MQConfig {
     @Bean
     public Queue queue() {
         return new Queue(QUEUE, true);
+    }
+
+    /**
+     * Topic Exchange
+     *
+     * @return
+     */
+    @Bean
+    public Queue topicQueue1() {
+        return new Queue(TOPIC_QUEUE1, true);
+    }
+
+    @Bean
+    public Queue topicQueue2() {
+        return new Queue(TOPIC_QUEUE2, true);
+    }
+
+    @Bean
+    public TopicExchange topicExchange() {
+        return new TopicExchange(TOPIC_EXCHANGE);
+    }
+
+    @Bean
+    public Binding topicBinding1() {
+        return BindingBuilder.bind(topicQueue1()).to(topicExchange()).with(ROUTING_KEY1);
+    }
+
+    @Bean
+    public Binding topicBinding2() {
+        return BindingBuilder.bind(topicQueue2()).to(topicExchange()).with(ROUTING_KEY2);
     }
 }
