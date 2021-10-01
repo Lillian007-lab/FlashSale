ac42ec6ef437046cdfc9408b6c72384ce89edadf
Tianyi Zhou <zcarol.530@gmail.com>
2021-10-01 17:08:16 -0400

    added methods to generate and verify path

--- src/main/java/com/example/flashsale/service/FlashSaleService.java
+++ src/main/java/com/example/flashsale/service/FlashSaleService.java
@@ -7,12 +7,14 @@ import com.example.flashsale.domain.Order;
 import com.example.flashsale.domain.Product;
 import com.example.flashsale.redis.FlashSaleKey;
 import com.example.flashsale.redis.RedisService;
+import com.example.flashsale.util.MD5Util;
 import com.example.flashsale.vo.ProductVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class FlashSaleService {
@@ -82,5 +84,34 @@ public class FlashSaleService {
     public void reset(List<ProductVo> productVoList) {
         productService.resetStock(productVoList);
         orderService.deleteOrders();
+    }
+
+    /**
+     * Generate UUID path
+     *
+     * @param user
+     * @param productId
+     * @return
+     */
+    public String createFlashSalePath(FlashSaleUser user, long productId) {
+        String str = MD5Util.md5(UUID.randomUUID() + "qwer");
+        redisService.set(FlashSaleKey.getFlashSalePath, "" + user.getId() + "_" + productId, str);
+        return str;
+    }
+
+    /**
+     * Verify path
+     *
+     * @param user
+     * @param productId
+     * @param path
+     * @return
+     */
+    public boolean checkPath(FlashSaleUser user, long productId, String path) {
+        if (user == null || path == null) {
+            return false;
+        }
+        String pathFromRedis = redisService.get(FlashSaleKey.getFlashSalePath, "" + user.getId() + "_" + productId, String.class);
+        return pathFromRedis.equals(path);
     }
 }
