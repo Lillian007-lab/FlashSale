ff2fbc01f37550917b670ea134719cceb390bc45
Tianyi Zhou <zcarol.530@gmail.com>
2021-10-01 01:15:35 -0400

    added FlashSaleKey

--- src/main/java/com/example/flashsale/redis/FlashSaleKey.java
+++ src/main/java/com/example/flashsale/redis/FlashSaleKey.java
@@ -1,2 +1,10 @@
-package com.example.flashsale.redis;public class FlashSaleKey {
+package com.example.flashsale.redis;
+
+public class FlashSaleKey extends BasePrefix{
+
+    public FlashSaleKey(String prefix) {
+        super(prefix);
+    }
+
+    public static FlashSaleKey isProductOver = new FlashSaleKey("productOver");
 }
