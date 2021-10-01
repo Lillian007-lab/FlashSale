220a0435151a99fc16f1de9396827b5d8c33359e
Tianyi Zhou <zcarol.530@gmail.com>
2021-10-01 17:06:18 -0400

    added getFlashSalePath key

--- src/main/java/com/example/flashsale/redis/FlashSaleKey.java
+++ src/main/java/com/example/flashsale/redis/FlashSaleKey.java
@@ -6,5 +6,11 @@ public class FlashSaleKey extends BasePrefix{
         super(prefix);
     }
 
+    public FlashSaleKey(int expireSeconds, String prefix) {
+        super(expireSeconds, prefix);
+    }
+
     public static FlashSaleKey isProductOver = new FlashSaleKey("productOver");
+    public static FlashSaleKey getFlashSalePath = new FlashSaleKey(60, "flashSalePath");
+
 }
