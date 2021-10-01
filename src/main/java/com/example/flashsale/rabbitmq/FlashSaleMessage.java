790a7e2396a859ac4f7ff388793fe1e02486ab2a
Tianyi Zhou <zcarol.530@gmail.com>
2021-10-01 01:19:23 -0400

    added FlashSaleMessage

--- src/main/java/com/example/flashsale/rabbitmq/FlashSaleMessage.java
+++ src/main/java/com/example/flashsale/rabbitmq/FlashSaleMessage.java
@@ -1,2 +1,25 @@
-package com.example.flashsale.rabbitmq;public class FlashSaleMessage {
+package com.example.flashsale.rabbitmq;
+
+import com.example.flashsale.domain.FlashSaleUser;
+
+public class FlashSaleMessage {
+
+    private FlashSaleUser user;
+    private long productId;
+
+    public FlashSaleUser getUser() {
+        return user;
+    }
+
+    public void setUser(FlashSaleUser user) {
+        this.user = user;
+    }
+
+    public long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(long productId) {
+        this.productId = productId;
+    }
 }
