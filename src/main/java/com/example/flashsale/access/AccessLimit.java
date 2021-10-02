6041e4ef937f4722f95e48b7890ff1e9de6788ed
Tianyi Zhou <zcarol.530@gmail.com>
2021-10-02 02:29:13 -0400

    added AccessLimit annotation

--- src/main/java/com/example/flashsale/access/AccessLimit.java
+++ src/main/java/com/example/flashsale/access/AccessLimit.java
@@ -1,2 +1,14 @@
-package com.example.flashsale.access;public @interface AccessLimit {
+package com.example.flashsale.access;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface AccessLimit {
+    int seconds();
+    int maxCount();
+    boolean needLogin() default true;
 }
