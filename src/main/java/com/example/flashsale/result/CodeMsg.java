0623b67de3a9735845e019493604476f3cdf4b90
Tianyi Zhou <zcarol.530@gmail.com>
2021-10-02 00:14:22 -0400

    updated code number and added FLASH_SALE_Failed code

--- src/main/java/com/example/flashsale/result/CodeMsg.java
+++ src/main/java/com/example/flashsale/result/CodeMsg.java
@@ -5,28 +5,29 @@ public class CodeMsg {
     private int code;
     private String msg;
 
-    // General code
+    // General code: 5001xx
     public static  CodeMsg SUCCESS = new CodeMsg(200, "success");
     public static  CodeMsg SERVER_ERROR = new CodeMsg(500100, "server error");
     public static  CodeMsg BIND_ERROR = new CodeMsg(500101, "Parameter error: %$");
     public static  CodeMsg REQUEST_ILLEGAL = new CodeMsg(500102, "Illegal request");
 
 
-    // Login code
-    public static CodeMsg SESSION_ERROR = new CodeMsg(500210, "Session does not exist or is expired");
-    public static CodeMsg PASSWORD_EMPTY = new CodeMsg(500211, "Password cannot be empty");
-    public static CodeMsg MOBILE_EMPTY = new CodeMsg(500212, "Mobile number cannot be empty");
-    public static CodeMsg MOBILE_ERROR = new CodeMsg(500213, "Mobile number format is incorrect");
-    public static CodeMsg MOBILE_NOT_EXIST  = new CodeMsg(500214, "Mobile number does not exist");
-    public static CodeMsg PASSWORD_ERROR  = new CodeMsg(500214, "Password is not correct");
+    // Login code: 5002xx
+    public static CodeMsg SESSION_ERROR = new CodeMsg(500200, "Session does not exist or is expired");
+    public static CodeMsg PASSWORD_EMPTY = new CodeMsg(500201, "Password cannot be empty");
+    public static CodeMsg MOBILE_EMPTY = new CodeMsg(500202, "Mobile number cannot be empty");
+    public static CodeMsg MOBILE_ERROR = new CodeMsg(500203, "Mobile number format is incorrect");
+    public static CodeMsg MOBILE_NOT_EXIST  = new CodeMsg(500204, "Mobile number does not exist");
+    public static CodeMsg PASSWORD_ERROR  = new CodeMsg(500205, "Password is not correct");
 
-    // Order code
-    public static final CodeMsg ORDER_NOT_EXIT = new CodeMsg(500214, "Order does not exit");;
+    // Order code: 5003xx
+    public static final CodeMsg ORDER_NOT_EXIT = new CodeMsg(500301, "Order does not exit");;
 
 
-    // Flash Sale code 5005xx
+    // Flash Sale code: 5005xx
     public static CodeMsg FLASH_SALE_OUT_OF_STOCK  = new CodeMsg(500500, "Product is out of stock");
-    public static CodeMsg FLASH_SALE_REPEAT  = new CodeMsg(500500, "Can't buy a same flash sale product multiple times");
+    public static CodeMsg FLASH_SALE_REPEAT  = new CodeMsg(500501, "Can't buy a same flash sale product multiple times");
+    public static CodeMsg FLASH_SALE_Failed  = new CodeMsg(500502, "Flash Sale Failed");
 
 
     public CodeMsg(int code, String msg) {
@@ -47,4 +48,5 @@ public class CodeMsg {
         String message = String.format(this.msg, args);
         return new CodeMsg(code, message);
     }
+
 }
